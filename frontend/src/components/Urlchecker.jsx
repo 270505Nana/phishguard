0a46ac6291a4ece44764c015da2b09627293f74e
270505Nana <prajna270505@gmail.com>
2026-06-16 12:13:28 +0700

    fix: import useEffect in UrlChecker

--- frontend/src/components/Urlchecker.jsx
+++ frontend/src/components/Urlchecker.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 // import function from validator.js
 import { isValidUrl, normalizeUrl } from '../utils/validator'
